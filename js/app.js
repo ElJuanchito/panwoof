ec2bd01dcf2be688767bd63619b36b3852829366
ElJuanchito <perdomocardenas18@gmail.com>
2024-05-17 01:09:27 -0500

    Agregando carrusel

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     const fila = document.createElement('tr');
                     fila.innerHTML = `
                         <td><img src="${producto.imagen}" alt="${producto.descripcion}" class="product-image img-fluid"></td>
-                        <td class="h6 align-middle" >${producto.descripcion} COP</td>
+                        <td class="h6 align-middle" >${producto.descripcion}</td>
                         <td class="h6 align-middle" >${producto.precio} COP</td>
                         <td class="align-middle" ><input type="number" min="0" class="form-control"/></td>
                         <td class="align-middle" ><input type="number" class="form-control"/></td>
